7180abb2bd63d512eb0b651b22983544b152b3ae
Nate <nate.rosenstock@hakai.org>
2025-03-20 11:06:33 -0700

    parse wnd_dir_code

--- swob_to_json/swob_to_json.py
+++ swob_to_json/swob_to_json.py
@@ -26,6 +26,44 @@ def parseFile(xml_file_path):
     return parseText(xml_string)
 
 
+def parse_ccg_wind_direction(value: int) -> float:
+    print(f"Parsing wind direction: {value}")
+    directions = {
+        "0": None,  # Calm
+        "1": 45,  # Northeast (NE)
+        "2": 90,  # East (E)
+        "3": 135,  # Southeast (SE)
+        "4": 180,  # South (S)
+        "5": 225,  # Southwest (SW)
+        "6": 270,  # West (W)
+        "7": 315,  # Northwest (NW)
+        "8": 0,  # North (N) - Changed from 360 to 0
+        "9": None,  # Variable/All directions/confused/unknown
+        "10": None,  # Not reported
+        "11": None,  # Ship in shore or flaw lead
+        "12": None,  # Not determined (ship in ice)
+        "13": None,  # Unable to report due to darkness, etc.
+        "14": 22.5,  # North-northeast (NNE)
+        "15": 67.5,  # East-northeast (ENE)
+        "16": 112.5,  # East-southeast (ESE)
+        "17": 157.5,  # South-southeast (SSE)
+        "18": 202.5,  # South-southwest (SSW)
+        "19": 247.5,  # West-southwest (WSW)
+        "20": 292.5,  # West-northwest (WNW)
+        "21": 337.5,  # North-northwest (NNW)
+    }
+
+    return directions.get(str(value), None)
+
+
+def is_integer_key(key: str) -> bool:
+    integer_keys = ["_code", "_summary"]
+    for integer_key in integer_keys:
+        if integer_key in key:
+            return True
+    return False
+
+
 def parseText(xml_string):
     """
     Converts XML to JSON
@@ -42,7 +80,11 @@ def parseText(xml_string):
     # convert results to numeric
     for key, value in record["results"].items():
         try:
-            record["results"][key] = float(value)
+            # code, flag, and summary are integers
+            if is_integer_key(key):
+                record["results"][key] = int(value)
+            else:
+                record["results"][key] = float(value)
         except:
             pass
 
@@ -55,4 +97,11 @@ def parseText(xml_string):
             pass
 
     replace_values(record["metadata"], "MSNG", None)
+
+    # check for ccg wind direction wnd_dir_code
+    # and convert to degrees
+    if "wnd_dir_code" in record["results"]:
+        record["results"]["wnd_dir"] = parse_ccg_wind_direction(
+            str(int(record["results"]["wnd_dir_code"]))
+        )
     return record
