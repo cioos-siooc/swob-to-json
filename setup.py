536b8351a812937e2ba5aa1bee54f3b26592a704
Nate <nate.rosenstock@hakai.org>
2025-03-20 11:10:24 -0700

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="swob_to_json",
-    version="0.1",
+    version="1",
     packages=["swob_to_json"],
     install_requires=["click", "xmltodict"],
     license="MIT",
